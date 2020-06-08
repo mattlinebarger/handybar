47b4f79d194212be657d79b8d729e0f4e9c4f694
Matt Linebarger <mlinebarger@gmail.com>
2020-06-08 13:28:16 -0500

    version 2
    Updated to Swift 5. Modifed Apache start/stop/restart to use brew version instead of default MacOS version.

--- HandyBar/AppDelegate.swift
+++ HandyBar/AppDelegate.swift
@@ -3,51 +3,52 @@
 //  HandyBar
 //
 //  Created by Matt Linebarger on 10/30/15.
-//  Copyright © 2015 Squabash. All rights reserved.
+//  Copyright © 2020 Ransom Labs. All rights reserved.
 //
 
 import Cocoa
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var statusMenu: NSMenu!
     
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         
         let icon = NSImage(named: "statusIcon")
-        icon?.template = true // ensures icon looks right in both light and dark mode
+        icon?.isTemplate = true // ensures icon looks right in both light and dark mode
         
-        statusItem.image = icon
+        statusItem.button?.image = icon
         statusItem.menu = statusMenu
         
     }
-
+    
     
     // Begin Finder Tasks ************************************************************
     
     // Toggle Hiddle Files Menu Item
-    @IBAction func toggleHiddenFiles(sender: NSMenuItem) {
+    @IBAction func toggleHiddenFiles(_ sender: NSMenuItem) {
         
-        let task = NSTask()
+        let task = Process()
         task.launchPath = "/usr/bin/defaults/"
         
-        if(sender.state == NSOnState) {
-            sender.state = NSOffState
+        if(sender.state == NSControl.StateValue.on) {
+            sender.state = NSControl.StateValue.off
             task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "NO"]
         } else {
-            sender.state = NSOnState
+            sender.state = NSControl.StateValue.on
             task.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "YES"]
         }
         
         task.launch()
         task.waitUntilExit()
         
-        let killtask = NSTask()
+        let killtask = Process()
         killtask.launchPath = "/usr/bin/killall"
         killtask.arguments = ["Finder"]
         killtask.launch()
@@ -55,28 +56,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     // Restart Finder Menu Item
-    @IBAction func restartFinder(sender: NSMenuItem) {
+    @IBAction func restartFinder(_ sender: NSMenuItem) {
         
         NSAppleScript(source: "do shell script \"killall -KILL Finder\"")!.executeAndReturnError(nil)
         
     }
     
     // Restart Dock Menu Item
-    @IBAction func restartDock(sender: NSMenuItem) {
+    @IBAction func restartDock(_ sender: NSMenuItem) {
         
         NSAppleScript(source: "do shell script \"killall -KILL Dock\"")!.executeAndReturnError(nil)
         
     }
     
     // Restart Menu Bar Menu Item
-    @IBAction func restartMenubar(sender: NSMenuItem) {
+    @IBAction func restartMenubar(_ sender: NSMenuItem) {
         
         NSAppleScript(source: "do shell script \"killall -KILL SystemUIServer\"")!.executeAndReturnError(nil)
         
     }
     
     // Show/Copy Internal IP Address Menu Item
-    @IBAction func showIntIP(sender: NSMenuItem) {
+    @IBAction func showIntIP(_ sender: NSMenuItem) {
         let intIP = NSAppleScript(source: "do shell script \"junkIP=$(ifconfig | grep 'inet ' | grep -v 127.0.0.1);ipArr=(${junkIP// / });thisIP=${ipArr[1]};echo $thisIP\"")!.executeAndReturnError(nil).stringValue
         
         let notification = NSUserNotification()
@@ -84,16 +85,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         notification.informativeText = "Internal IP Address: " + intIP! + "\r\nAddress has been copied to the clipboard."
         notification.soundName = NSUserNotificationDefaultSoundName
         
-        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
+        NSUserNotificationCenter.default.deliver(notification)
         
-        let pasteboard = NSPasteboard.generalPasteboard()
+        let pasteboard = NSPasteboard.general
         pasteboard.clearContents()
-        pasteboard.setString(intIP!, forType: NSPasteboardTypeString)
- 
+        pasteboard.setString(intIP!, forType: NSPasteboard.PasteboardType.string)
+        
     }
     
     // Show/Copy External IP Address Menu Item
-    @IBAction func showExtIP(sender: NSMenuItem) {
+    @IBAction func showExtIP(_ sender: NSMenuItem) {
         let extIP = NSAppleScript(source: "do shell script \"dig +short myip.opendns.com @resolver1.opendns.com\"")!.executeAndReturnError(nil).stringValue
         
         let notification = NSUserNotification()
@@ -101,43 +102,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         notification.informativeText = "External IP Address: " + extIP! + "\r\nAddress has been copied to the clipboard."
         notification.soundName = NSUserNotificationDefaultSoundName
         
-        NSUserNotificationCenter.defaultUserNotificationCenter().deliverNotification(notification)
+        NSUserNotificationCenter.default.deliver(notification)
         
-        let pasteboard = NSPasteboard.generalPasteboard()
+        let pasteboard = NSPasteboard.general
         pasteboard.clearContents()
-        pasteboard.setString(extIP!, forType: NSPasteboardTypeString)
+        pasteboard.setString(extIP!, forType: NSPasteboard.PasteboardType.string)
         
     }
     // End Finder Tasks **************************************************************
     
-    // Begin Apache Tasks ************************************************************
+    // Begin Apache Tasks (Brew Version/Not Default Apache) **************************
     
     // Start Apache Menu Item
-    @IBAction func apacheStart(sender: NSMenuItem) {
+    @IBAction func apacheStart(_ sender: NSMenuItem) {
         
-        NSAppleScript(source: "do shell script \"sudo apachectl start\" with administrator privileges")!.executeAndReturnError(nil)
+        NSAppleScript(source: "do shell script \"/usr/local/bin/apachectl start\"")!.executeAndReturnError(nil)
         
     }
     
     // Stop Apache Menu Item
-    @IBAction func apacheStop(sender: NSMenuItem) {
+    @IBAction func apacheStop(_ sender: NSMenuItem) {
         
-        NSAppleScript(source: "do shell script \"sudo apachectl stop\" with administrator privileges")!.executeAndReturnError(nil)
+        NSAppleScript(source: "do shell script \"/usr/local/bin/apachectl stop\"")!.executeAndReturnError(nil)
         
     }
     
     // Restart Apache Menu Item
-    @IBAction func apacheRestart(sender: NSMenuItem) {
+    @IBAction func apacheRestart(_ sender: NSMenuItem) {
         
-        NSAppleScript(source: "do shell script \"sudo apachectl -k restart\" with administrator privileges")!.executeAndReturnError(nil)
+        NSAppleScript(source: "do shell script \"/usr/local/bin/apachectl restart\"")!.executeAndReturnError(nil)
         
     }
-    // End Apache Tasks **************************************************************
+    // End Apache Tasks (Brew Version/Not Default Apache) ****************************
     
     
     // Quit Menu Item
-    @IBAction func quitClicked(sender: NSMenuItem) {
-        NSApplication.sharedApplication().terminate(self)
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared.terminate(self)
     }
     
 }
